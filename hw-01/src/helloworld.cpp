00ec4e465dafdcc9d35ab7bed4b1813eb55b9253
SuwakoMeguri <suwakomeguri@gmail.com>
2024-04-30 13:12:05 +0300

    change some stuff

---
+++ hw-01/src/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <lib.h>
+
+int main (int argc, char *argv[]) {
+  std::cout << version() << std::endl;
+  std::cout << "Hello, World" << sdt::endl;
+}
---
+++ hw-01/src/version.h
@@ -0,0 +1,3 @@
+#pragma once
+
+/* #undef BUILD_NUMBER */
